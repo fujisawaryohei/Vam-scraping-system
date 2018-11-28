4d05630842d42729f91ea7c2810ba69d018a3f00
vagrant <vagrant@localhost.localdomain>
2018-11-28 19:32:57 +0900

    IPO銘柄スクレイピングのための文字列処理まで完了

--- app.js
+++ app.js
@@ -21,46 +21,47 @@ app.use(cookieParser());
 app.listen(3000,function(){
   console.log("Listening on 3000");
 });
-/*
-var options = {
-  url:'https://www.sejuku.net/blog/73698#request',
-  method:'GET'
+
+const start = '<tr bgcolor="#ffffff">';
+
+const singlehtmlTag = ['<td width="100" align="center">','</br>','<td width="50" align="center">','<td width="160">','<td width="120" align="center">',];
+
+const closeTag = ['<br/>','</td>','</td>','<br/>','<br/>'];
+
+const url = 'https://ipoget.com/ipodiary/%E3%82%BD%E3%83%95%E3%83%88%E3%83%90%E3%83%B3%E3%82%AF%EF%BC%889434%EF%BC%89ipo%EF%BC%88%E6%96%B0%E8%A6%8F%E4%B8%8A%E5%A0%B4%EF%BC%89%E3%81%AB%E5%BD%93%E9%81%B8%E3%81%99%E3%82%8B%E3%81%9F%E3%82%81/'
+
+const dom = client.fetch(url);
+
+dom.then((result)=>{
+  const count = (result.body.match(new RegExp(start,"g"))||[]).length;//この回数分繰り返す。
+  singleData(result);
+  doubleData(result);
+});
+
+//普通のデータをとるときの文字列処理
+//forEachで回す
+const singleData = (result)=>{ //配列
+  let html = result.body;
+  let startIndex = html.indexOf(start);//16570
+  //ここの第2引数にfinal_countで</tr>のindexを受け渡す。
+  let dayHtml=singlehtmlTag[i];
+  let dayIndex = html.indexOf(dayHtml,startIndex);
+  let closeTagIndex = html.indexOf(closeTag[i],dayIndex);
+  console.log(html.substring(dayIndex+dayHtml.length,closeTagIndex));
+  console.log(html.substring(dayIndex+day));
+};
+
+//hrefタグの文字列処理
+//forEachで回す
+const stockName = (result)=>{
+  let html = result.body;
+  let startIndex = html.indexOf(start);
+  let hrefHtml = '<a href=';
+  let hrefIndex = html.indexOf(hrefHtml,startIndex);
+  let closeTagIndex = html.indexOf('</a>',hrefIndex);
+  let href = '<a href="https://ipoget.com/?p=qqqqq" target="_blank">';
+  console.log(html.substring(hrefIndex+href.length,closeTagIndex));
 }
-*/
-
-//第１引数にURL,第2引数にquery,第3引数コールバック関数
-var html = ['<td class="tac">','<td class="tal">','<td>','<td width="61">','<td width="50">'];
-
-var cheerio = client.fetch('https://kabutan.jp/warning/?mode=3_1');
- cheerio.then((result)=>{
-   stringNormal(result);
-   stringHref(result);
- });
-
-  function stringNormal(result){//銘柄名
-    var body = result.body;
-    var htmlIndex = body.indexOf(html[1]);
-    var htmlLength = html[1].length;
-    var closetagIndex = body.indexOf('</td>',htmlIndex);
-    var stockname = body.substring(htmlIndex+htmlLength,closetagIndex);
-
-    var stockhtml = body.indexOf(html[2],htmlIndex);
-    var a = body.indexOf('</a>',stockhtml)
-    var stockvalue = body.substring(stockhtml,a);
-    console.log(stockname + ":" + stockvalue);
-  }
-
-  function stringHref(result){//株価コード
-    var body = result.body;
-    var htmlIndex = body.indexOf(html[0]);
-    var href = '<a href="/stock/?code=????>"';
-    var closetagIndex = body.indexOf('</a>',htmlIndex);
-    var b = body.substring(htmlIndex+href.length+16,closetagIndex);
-  }
-
-  function stringSpan(){
-
-  }
 
 app.use(function(req, res, next) {
   next(createError(404));
