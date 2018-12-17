756b1348918509c5f9af82783c50c61aa4669737
vagrant <vagrant@localhost.localdomain>
2018-12-17 18:56:07 +0900

    stocknameのAPI完成

--- app.js
+++ app.js
@@ -3,18 +3,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var client = require('cheerio-httpcli');
 var request = require('request-promise');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var highPrice = require('./highPrice'); //ファイルモジュール呼び出し
 var app = express();
 const {google} = require('googleapis');
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'jade');
-//app.use(express.static(path.join(__dirname, 'public')));
-//app.use('/', indexRouter);
-//app.use('/users', usersRouter);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -23,76 +22,95 @@ app.listen(3000,function(){
   console.log("Listening on 3000-------------------------------");
 });
 
-//スクレイピングに必要な情報の定義
-var url = 'http://www.miller.co.jp/applications/cgi-bin/cv0/rnk20/01/cv0rnk20c.cgi?_hps=off&id=4';
-var start = '<td class="tLeft rkgSelected01">';
-var arrayBody = new Array;
-var htmlBody = new String;
-var stockArray = new Array;
+app.get('/stockname',(req,res)=>{
+  highPrice.stockNameArray(highPrice.url,highPrice.extractName).then(value=>{
+    res.send(value);
+  }).catch(console.log("err!"));
+});
 
-//stockArrayは銘柄HTML要素の配列を返す //第2引数では、抽出処理している関数を渡す
-function stockNameArray(link,callback) {
-  var dom = client.fetch(link);
-    dom.then((res)=>{
-      var body = res.body;
-      var count = (body.match(new RegExp(start,"g"))||[]).length;
-        for(var i=1;i <= count;i++) {
-          var htmlTag = `<td class="">${i}</td>`;
-          var startIndex = body.indexOf(htmlTag);
-          var closeIndex = body.indexOf('</tr>',startIndex);
-          htmlBody = body.substring(startIndex,closeIndex);
-          stockArray.push(htmlBody); //stockArrayが銘柄の配列.
-        }
-        callback(stockArray);
-    });
-}
+// async stockname(){
+//   var stock = await highPrice.stockNameArray(highPrice.url,highPrice.extractName);
+// }.then((stock)=>{
+//   console.log(stock);
+// });
 
-//新高値銘柄と銘柄コード抽出
-function extractName(array){
-  for(var arr of array){
-    var startIndex = arr.indexOf(start);
-    var closeTag = arr.indexOf('</a>',startIndex);
-    var href = '<a href="/chart.cgi?????TB" target="_chart">';
-    var result = arr.substring(start.length + startIndex + href.length, closeTag)
-    console.log(result);
-  }
-}
-
-//市場と業種の取得
-function extractMarket(array){
-  for(var arr of array){
-    var startIndex = arr.indexOf(start);
-    var htmlTag = '<td class="tLeft ">';
-    var htmlIndex = arr.indexOf(htmlTag,startIndex);
-    var closeTag = ['<br />','</td>']
-    var closeIndex1 = arr.indexOf(closeTag[0],htmlIndex);
-    var closeIndex2 = arr.indexOf(closeTag[1],closeIndex1);
-    console.log(arr.substring(htmlIndex + htmlTag.length , closeIndex1));     console.log(arr.substring(htmlIndex + htmlTag.length + closeTag[1].length + closeTag[0].length - 1 , closeIndex2));
-  }
-}
-
-//終値を取得
-function extractFinalBalance(array){
-  for(var arr of array){
-    var startIndex1 = arr.indexOf(start);
-    var htmlTag1 = '<td class="tRight " >';
-    var htmlTag2 = '<strong>';
-    var htmlIndex1 = arr.indexOf(htmlTag1,startIndex1);
-    var htmlIndex2 = arr.indexOf(htmlTag2,htmlIndex1);
-    var closeTagIndex1 = arr.indexOf('<span',htmlIndex1);
-    var closeTagIndex2 = arr.indexOf('</strong>',htmlIndex2);
-     var updown = arr.substring(htmlIndex2 + htmlTag2.length,closeTagIndex2);
-     console.log(arr.substring(htmlIndex1 + htmlTag1.length,closeTagIndex1)+updown);
-  }
-}
-
-// 前日比
+app.use(function(req,res,next){
+  var err = new Error('Not Found');
+  err.status=404;
+  next(err);
+})
 
 
-// 出来高(千株)は後で
+// //スクレイピングに必要な情報の定義
+// var url = 'http://www.miller.co.jp/applications/cgi-bin/cv0/rnk20/01/cv0rnk20c.cgi?_hps=off&id=4';
+// var start = '<td class="tLeft rkgSelected01">';
+// var arrayBody = new Array;
+// var htmlBody = new String;
+// var stockArray = new Array;
+//
+// //stockArrayは銘柄HTML要素の配列を返す //第2引数では、抽出処理している関数を渡す
+// function stockNameArray(link,callback) {
+//   var dom = client.fetch(link);
+//     dom.then((res)=>{
+//       var body = res.body;
+//       var count = (body.match(new RegExp(start,"g"))||[]).length;
+//         for(var i=1;i <= count;i++) {
+//           var htmlTag = `<td class="">${i}</td>`;
+//           var startIndex = body.indexOf(htmlTag);
+//           var closeIndex = body.indexOf('</tr>',startIndex);
+//           htmlBody = body.substring(startIndex,closeIndex);
+//           stockArray.push(htmlBody); //stockArrayが銘柄の配列.
+//         }
+//         callback(stockArray);
+//     });
+// }
+//
+// stockNameArray(url,extractName)
+// //新高値銘柄と銘柄コード抽出
+// function extractName(array){
+//   for(var arr of array){
+//     var startIndex = arr.indexOf(start);
+//     var closeTag = arr.indexOf('</a>',startIndex);
+//     var href = '<a href="/chart.cgi?????TB" target="_chart">';
+//     var result = arr.substring(start.length + startIndex + href.length, closeTag)
+//     return result;
+//   }
+// }
+//
+// //市場と業種の取得
+// function extractMarket(array){
+//   for(var arr of array){
+//     var startIndex = arr.indexOf(start);
+//     var htmlTag = '<td class="tLeft ">';
+//     var htmlIndex = arr.indexOf(htmlTag,startIndex);
+//     var closeTag = ['<br />','</td>']
+//     var closeIndex1 = arr.indexOf(closeTag[0],htmlIndex);
+//     var closeIndex2 = arr.indexOf(closeTag[1],closeIndex1);
+//     console.log(arr.substring(htmlIndex + htmlTag.length , closeIndex1));     console.log(arr.substring(htmlIndex + htmlTag.length + closeTag[1].length + closeTag[0].length - 1 , closeIndex2));
+//   }
+// }
+//
+// //終値を取得
+// function extractFinalBalance(array){
+//   for(var arr of array){
+//     var startIndex1 = arr.indexOf(start);
+//     var htmlTag1 = '<td class="tRight " >';
+//     var htmlTag2 = '<strong>';
+//     var htmlIndex1 = arr.indexOf(htmlTag1,startIndex1);
+//     var htmlIndex2 = arr.indexOf(htmlTag2,htmlIndex1);
+//     var closeTagIndex1 = arr.indexOf('<span',htmlIndex1);
+//     var closeTagIndex2 = arr.indexOf('</strong>',htmlIndex2);
+//      var updown = arr.substring(htmlIndex2 + htmlTag2.length,closeTagIndex2);
+//      console.log(arr.substring(htmlIndex1 + htmlTag1.length,closeTagIndex1)+updown);
+//   }
+// }
+//
+// // 前日比
+//
+//
+// // 出来高(千株)は後で
 
 
-console.log();
 app.use(function(req, res, next) {
   next(createError(404));
 });
--- highPrice.js
+++ highPrice.js
@@ -1,68 +1,73 @@
-// //スクレイピングに必要な情報の定義
-// var url = 'http://www.miller.co.jp/applications/cgi-bin/cv0/rnk20/01/cv0rnk20c.cgi?_hps=off&id=4';
-// var start = '<td class="tLeft rkgSelected01">';
-// var arrayBody = new Array;
-// var htmlBody = new String;
-// var stockArray = new Array;
-//
-// //stockArrayは銘柄HTML要素の配列を返す //第2引数では、抽出処理している関数を渡す
-// function stockNameArray(link,callback) {
-//   var dom = client.fetch(link);
-//     dom.then((res)=>{
-//       var body = res.body;
-//       var count = (body.match(new RegExp(start,"g"))||[]).length;
-//         for(var i=1;i <= count;i++) {
-//           var htmlTag = `<td class="">${i}</td>`;
-//           var startIndex = body.indexOf(htmlTag);
-//           var closeIndex = body.indexOf('</tr>',startIndex);
-//           htmlBody = body.substring(startIndex,closeIndex);
-//           stockArray.push(htmlBody); //stockArrayが銘柄の配列.
-//         }
-//         callback(stockArray);
-//     });
-// }
-//
-// //新高値銘柄と銘柄コード抽出
-// function extractName(array){
-//   for(var arr of array){
-//     var startIndex = arr.indexOf(start);
-//     var closeTag = arr.indexOf('</a>',startIndex);
-//     var href = '<a href="/chart.cgi?????TB" target="_chart">';
-//     var result = arr.substring(start.length + startIndex + href.length, closeTag)
-//     console.log(result);
-//   }
-// }
-//
-// //市場と業種の取得
-// function extractMarket(array){
-//   for(var arr of array){
-//     var startIndex = arr.indexOf(start);
-//     var htmlTag = '<td class="tLeft ">';
-//     var htmlIndex = arr.indexOf(htmlTag,startIndex);
-//     var closeTag = ['<br />','</td>']
-//     var closeIndex1 = arr.indexOf(closeTag[0],htmlIndex);
-//     var closeIndex2 = arr.indexOf(closeTag[1],closeIndex1);
-//     console.log(arr.substring(htmlIndex + htmlTag.length , closeIndex1));     console.log(arr.substring(htmlIndex + htmlTag.length + closeTag[1].length + closeTag[0].length - 1 , closeIndex2));
-//   }
-// }
-//
-// //終値を取得
-// function extractFinalBalance(array){
-//   for(var arr of array){
-//     var startIndex1 = arr.indexOf(start);
-//     var htmlTag1 = '<td class="tRight " >';
-//     var htmlTag2 = '<strong>';
-//     var htmlIndex1 = arr.indexOf(htmlTag1,startIndex1);
-//     var htmlIndex2 = arr.indexOf(htmlTag2,htmlIndex1);
-//     var closeTagIndex1 = arr.indexOf('<span',htmlIndex1);
-//     var closeTagIndex2 = arr.indexOf('</strong>',htmlIndex2);
-//      var updown = arr.substring(htmlIndex2 + htmlTag2.length,closeTagIndex2);
-//      console.log(arr.substring(htmlIndex1 + htmlTag1.length,closeTagIndex1)+updown);
-//   }
-// }
-//
-// stockNameArray(url,extractName)
-// // 前日比
-//
-//
-// // 出来高(千株)は後で
+//スクレイピングに必要な情報の定義
+var client = require('cheerio-httpcli');
+module.exports.url = 'http://www.miller.co.jp/applications/cgi-bin/cv0/rnk20/01/cv0rnk20c.cgi?_hps=off&id=4';
+var start = '<td class="tLeft rkgSelected01">';
+var arrayBody = new Array;
+var htmlBody = new String;
+var stockArray = new Array;
+
+//stockArrayは銘柄HTML要素の配列を返す //第2引数では、抽出処理している関数を渡す
+exports.stockNameArray = function(link,callback) {
+  return new Promise(resolve=>{
+    var dom = client.fetch(link);
+      dom.then((res)=>{
+        var body = res.body;
+        var count = (body.match(new RegExp(start,"g"))||[]).length;
+          for(var i=1;i <= count;i++) {
+            var htmlTag = `<td class="">${i}</td>`;
+            var startIndex = body.indexOf(htmlTag);
+            var closeIndex = body.indexOf('</tr>',startIndex);
+            htmlBody = body.substring(startIndex,closeIndex);
+            stockArray.push(htmlBody); //stockArrayが銘柄の配列.
+          }
+          resolve(callback(stockArray));
+      });
+  })
+}
+
+//新高値銘柄と銘柄コード抽出
+exports.extractName = function(array){
+  var results = [];
+  for(var arr of array){
+    var startIndex = arr.indexOf(start);
+    var closeTag = arr.indexOf('</a>',startIndex);
+    var href = '<a href="/chart.cgi?????TB" target="_chart">';
+    var result = arr.substring(start.length + startIndex + href.length, closeTag);
+    results.push(result);
+  }
+   return JSON.stringify({name:results})
+  }
+
+
+//市場と業種の取得
+exports.extractMarket = function(array){
+  for(var arr of array){
+    var startIndex = arr.indexOf(start);
+    var htmlTag = '<td class="tLeft ">';
+    var htmlIndex = arr.indexOf(htmlTag,startIndex);
+    var closeTag = ['<br />','</td>']
+    var closeIndex1 = arr.indexOf(closeTag[0],htmlIndex);
+    var closeIndex2 = arr.indexOf(closeTag[1],closeIndex1);
+    console.log(arr.substring(htmlIndex + htmlTag.length , closeIndex1));     console.log(arr.substring(htmlIndex + htmlTag.length + closeTag[1].length + closeTag[0].length - 1 , closeIndex2));
+  }
+}
+
+//終値を取得
+exports.extractFinalBalance = function(array){
+  for(var arr of array){
+    var startIndex1 = arr.indexOf(start);
+    var htmlTag1 = '<td class="tRight " >';
+    var htmlTag2 = '<strong>';
+    var htmlIndex1 = arr.indexOf(htmlTag1,startIndex1);
+    var htmlIndex2 = arr.indexOf(htmlTag2,htmlIndex1);
+    var closeTagIndex1 = arr.indexOf('<span',htmlIndex1);
+    var closeTagIndex2 = arr.indexOf('</strong>',htmlIndex2);
+     var updown = arr.substring(htmlIndex2 + htmlTag2.length,closeTagIndex2);
+     console.log(arr.substring(htmlIndex1 + htmlTag1.length,closeTagIndex1)+updown);
+  }
+}
+
+// 前日比
+
+
+// 出来高(千株)は後で
